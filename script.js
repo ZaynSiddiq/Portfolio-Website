219de02f3d3cbc3597c7c56c6c3563855a759bed
ZaynSiddiq <45522254+ZaynSiddiq@users.noreply.github.com>
2021-07-30 10:11:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//script
+function onClick()
+{
+  if(document.getElementById("title").style.color == "white")
+  {
+    document.getElementById("title").style.color = "blue";
+  }
+  else{
+    document.getElementById("title").style.color = "white";
+  }
+}
